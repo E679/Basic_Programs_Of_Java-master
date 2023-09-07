c9076e3f983de7a12b37c64965e6e089c52fcecf
Supreeth Goud <saisupreethgoud4@gmail.com>
2023-09-07 22:49:28 +0530

    Added split method

--- Basic_Programs_Of_Java-master/src/temp/tempclass.java
+++ Basic_Programs_Of_Java-master/src/temp/tempclass.java
@@ -1,12 +1,17 @@
 package temp;
 
+import java.util.Arrays;
+
 public class tempclass {
     public static void main(String[] args) {
-        String s="SuprEEth Goud";
+        String s="SuprEEth:Goud";
 
-        //s.split(" ");
+        String s2= s.split(":")[0];
+        String s3= s.split(":")[1];
 
-        System.out.println(s.replace("S","s"));
-        System.out.println(s.replaceAll("E","e"));
+        System.out.println(s2.replace("S","s"));
+        System.out.println(s2.replaceAll("E","e"));
+        System.out.println("splitted string arr[0] is "+s2);
+        System.out.println("splitted string arr[1] is "+s3);
     }
 }
