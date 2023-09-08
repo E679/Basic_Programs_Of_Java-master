26e83acf328b090b6596957de31e35f56c550f3c
Supreeth Goud <saisupreethgoud4@gmail.com>
2023-09-08 12:24:51 +0530

    GroupWordsByLength and get employee salary incremented

--- Basic_Programs_Of_Java-master/src/temp/GroupWordsByLength.java
+++ Basic_Programs_Of_Java-master/src/temp/GroupWordsByLength.java
@@ -1,6 +1,8 @@
 package temp;
 
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,6 +12,26 @@ public class GroupWordsByLength {
         List<String> words= Arrays.asList("I","have","a","sentence","with","technical","words");
 
         Map<Integer, List<String>> result=words.stream().collect(Collectors.groupingBy(String::length));
-        System.out.println(result);
+        System.out.println("stream result "+result);
+        System.out.println("Normal output "+getgroupwords(words));
+    }
+
+    private static Map<Integer, List<String>> getgroupwords(List<String> words){
+        Map<Integer, List<String>> result1=new HashMap<>();
+        for (String word:words){
+            int length=word.length();
+            if (result1.containsKey(length)){
+                List<String> rs1 = result1.get(length);
+                rs1.add(word);
+                result1.put(length,rs1);
+            } else {
+                List<String> rs = new ArrayList<>();
+                //Add string
+                rs.add(word);
+                //Add key and list to map
+                result1.put(length, rs);
+            }
+        }
+        return result1;
     }
 }
