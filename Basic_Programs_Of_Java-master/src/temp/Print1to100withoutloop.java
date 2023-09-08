e97fccd03d239ba3b476293189db5e923229e49f
Supreeth Goud <saisupreethgoud4@gmail.com>
2023-09-08 22:48:51 +0530

    Print1to100withoutloop

--- Basic_Programs_Of_Java-master/src/temp/Print1to100withoutloop.java
+++ Basic_Programs_Of_Java-master/src/temp/Print1to100withoutloop.java
@@ -4,6 +4,8 @@ public class Print1to100withoutloop {
     public static void main(String[] args) {
         int n=1;
         getnumsfrom1to100withoutanyloop(n);
+        String s=1+2+"abc"+1+2+"def";
+        System.out.println("string value"+s);// output: 3abc12def// if string starts with integers it starts adding and once it encounters with character it starts concatenating till end
     }
     private static void getnumsfrom1to100withoutanyloop(int n){
         if(n<=100){
