e8da6a703221365ea7b33fcf3ed84b2ec543c4d9
Supreeth Goud <saisupreethgoud4@gmail.com>
2023-12-19 23:14:01 +0530

    StringEqualsMethod

--- Basic_Programs_Of_Java-master/src/ReverseString/StringEqualsMethod.java
+++ Basic_Programs_Of_Java-master/src/ReverseString/StringEqualsMethod.java
@@ -2,7 +2,7 @@ package ReverseString;
 
 public class StringEqualsMethod {
     public static void main(String[] args) {
-        
+
         String s1 = new String("QuickRef");
         String s2 = new String("QuickRef");
 
@@ -15,5 +15,19 @@ public class StringEqualsMethod {
         System.out.println("s1.equals(s3) value is: "+(s1.equals(s3))); //true
         System.out.println("s1.equals(s4) value is: "+(s1.equals(s4))); //false
         System.out.println("s3==s5 value is: "+(s3==s5));   //true
+
+        String str = "abcd";
+
+        System.out.println(str.charAt(2));       // c
+        System.out.println(str.indexOf("a"));    // 0
+        System.out.println(str.indexOf("z"));    // -1
+        System.out.println(str.length());        // 4
+        System.out.println(str.toString());      // abcd
+        System.out.println("Substring with one begin index "+str.substring(2));    // cd
+        System.out.println("Substring with two indices begin and end "+str.substring(2,3));  // c
+        System.out.println(str.contains("c"));   // true
+        System.out.println(str.endsWith("d"));   // true
+        System.out.println(str.startsWith("a")); // true
+        System.out.println(str.isEmpty());       // false
     }
 }
