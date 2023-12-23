91796c2e01a7ec73ec6bd5c88dea94966822b9be
Supreeth Goud <saisupreethgoud4@gmail.com>
2023-12-23 23:15:27 +0530

    FormalReverseArray

--- Basic_Programs_Of_Java-master/src/ReverseArray/FormalReverseArray.java
+++ Basic_Programs_Of_Java-master/src/ReverseArray/FormalReverseArray.java
@@ -4,18 +4,43 @@ public class FormalReverseArray {
 
         public static void main(String[] args) {
             int[] array = {1, 2, 3, 4, 5};
+            int[] array1 = {10,9,8,7,6,5};
 
             System.out.println("Original Array:");
             printArray(array);
 
-            reverseArray(array);
-
-            System.out.println("\nReversed Array:");
+            System.out.println("\nReversed Array Using While Loop: \n");
+            reverseUsingWhileLoop(array);
             printArray(array);
+            System.out.println("\nReversed Array Using For Loop: \n");
+            reverseUsingForLoop(array1);
+
+//            System.out.println("\nReversed Array: \n");
+//            printArray(array);
         }
 
-        // Method to reverse the array
-        private static void reverseArray(int[] arr) {
+    private static void reverseUsingForLoop(int[] array) {
+        for(int i=array.length - 1; i>=0; i--){
+            System.out.print(array[i]+" ");
+        }
+        /*
+        Another way
+        store in new array and send the new array
+         */
+
+//        int[] res=new int[array.length];
+//        int j=0;
+//        for(int i=array.length - 1; i>=0; i--){
+//            res[j++]=array[i];
+//        }
+//        //printing the new array within index j length
+//        for(int i=0; i<j; i++){
+//            System.out.print(res[i]+" ");
+//        }
+    }
+
+    // Method to reverse the array
+        private static void reverseUsingWhileLoop(int[] arr) {
             int start = 0;
             int end = arr.length - 1;
 
