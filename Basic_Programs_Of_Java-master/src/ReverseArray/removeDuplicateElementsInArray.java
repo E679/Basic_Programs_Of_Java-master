0bd17d112f476bbad2220e8bcdce761d6a31067f
Supreeth Goud <saisupreethgoud4@gmail.com>
2023-12-23 21:06:55 +0530

    removeDuplicateElementsInArray

--- Basic_Programs_Of_Java-master/src/ReverseArray/removeDuplicateElementsInArray.java
+++ Basic_Programs_Of_Java-master/src/ReverseArray/removeDuplicateElementsInArray.java
@@ -3,6 +3,9 @@ package ReverseArray;
 // Java Program to Remove Duplicate Elements
 // From the Array using extra space
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class removeDuplicateElementsInArray {
 
     public static int removeduplicates(int a[], int n)
@@ -40,8 +43,56 @@ public class removeDuplicateElementsInArray {
         n = removeduplicates(a, n);
 
         // Printing The array elements
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             System.out.print(a[i] + " ");
+        }
+
+        //Printing The array elements Using Set
+
+        int[] arr={5,48,56,12,4,4,5,5,48};
+        int len= arr.length;
+
+        len=removeduplicatesUsingSet(arr,len);//48 4 5 56 12
+
+        arr=sortedArray(arr,len);//4 5 12 48 56
+
+        System.out.print("From set: \n");
+        for (int i =0;i<len;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    private static int[] sortedArray(int[] arr, int len) {
+        int temp=0;
+        for (int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    private static int removeduplicatesUsingSet(int[] arr, int len) {
+
+        if(len<=1){
+            return len;
+        }
+        Set<Integer> uniqueElements=new HashSet<>();
+
+        for(int i=0;i<len;++i){
+            uniqueElements.add(arr[i]);
+        }
+
+//        int j=0;
+//        for(Integer a:uniqueElements){
+//                arr[j++]=a;
+//        }
+
+        return uniqueElements.size();
     }
 }
 
