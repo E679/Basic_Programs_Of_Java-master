2c510f6e5d249ade8d4e92e48b8ee6ef95e2c923
Supreeth Goud <saisupreethgoud4@gmail.com>
2024-06-29 20:54:00 +0530

    PalindromeRecursive and nonrecursive

--- Basic_Programs_Of_Java-master/src/PalindromeForString/PalindromeRecursive.java
+++ Basic_Programs_Of_Java-master/src/PalindromeForString/PalindromeRecursive.java
@@ -2,7 +2,10 @@ package PalindromeForString;
 
 public class PalindromeRecursive {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("ab1221ba"));
+        boolean givenString = isPalindrome("ab1221ba");
+        boolean withoutRecursive = testPalindromeWithNonRecursive("ab1221ba");
+        System.out.println("Given String Is a Palindrome with Recursive method");
+        System.out.println("Given String Is a Palindrome without Recursive method");
     }
 
     private static boolean isPalindrome(String text){
@@ -14,7 +17,20 @@ public class PalindromeRecursive {
         if(text == null || text.isEmpty()){
             return text;
         }
-        return text.charAt(text.length()-1)+
-                palindrome(text.substring(0,text.length()-1));
+        String result= text.charAt(text.length() - 1) + palindrome(text.substring(0, text.length() - 1));
+        return result;
+    }
+    private static boolean testPalindromeWithNonRecursive(String text){
+        if(text == null || text.isEmpty()){
+            return false;
+        }
+        String result="";
+        for(int i=text.length()-1;i>=0;i--){
+            result+=text.charAt(i);
+        }
+        if(result.equals(text)){
+            return true;
+        }
+        return false;
     }
 }
