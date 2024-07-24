83f5766f77e4a8e1730576ef48419ec523ed1b9e
Supreeth Goud <saisupreethgoud4@gmail.com>
2024-07-24 13:58:28 +0530

    BinarySearch

--- Basic_Programs_Of_Java-master/src/BinarySearch/BinarySearch.java
+++ Basic_Programs_Of_Java-master/src/BinarySearch/BinarySearch.java
@@ -25,6 +25,31 @@ public class BinarySearch {
         }
     }
 
+    // Returns index of x if it is present in arr[].
+    static int binarySearch(int arr[], int x)
+    {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if x is present at mid
+            if (arr[mid] == x)
+                return mid;
+
+            // If x greater, ignore left half
+            if (arr[mid] < x)
+                low = mid + 1;
+
+                // If x is smaller, ignore right half
+            else
+                high = mid - 1;
+        }
+
+        // If we reach here, then element was
+        // not present
+        return -1;
+    }
+
     public static void main(String[] args) {
         int arr[]={20,10,50,30,40};
         Arrays.sort(arr);//In binary search array should be in sorted order for sure before searching
@@ -33,6 +58,18 @@ public class BinarySearch {
         System.out.println("Enter key to search");
         int key=sc.nextInt();
         binarySearch(arr,0,last,key);
+
+        //Another way easy
+        int arr1[] = { 2, 3, 4, 10, 40 };
+        int searchElement = 10;
+        int result = binarySearch(arr1, searchElement);
+        if (result == -1)
+            System.out.println(
+                    "Element is not present in array");
+        else
+            System.out.println("Element is present at "
+                    + "index " + result);
+    }
     }
 
 }
