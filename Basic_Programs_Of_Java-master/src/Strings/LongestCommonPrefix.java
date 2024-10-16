19f5c2f83e48e44e07a85ed2c9af1d9bcf5388fb
Supreeth <saisupreethgoud4@gmail.com>
2024-10-16 17:56:17 +0530

    LongestCommonPrefix

--- Basic_Programs_Of_Java-master/src/Strings/LongestCommonPrefix.java
+++ Basic_Programs_Of_Java-master/src/Strings/LongestCommonPrefix.java
@@ -1,6 +1,7 @@
 package Strings;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /*
        Write a function to find the longest common prefix string amongst an array of strings.
@@ -61,6 +62,46 @@ public class LongestCommonPrefix {
             }
         }
         System.out.println("Ouptut prefix: "+result);
+
+        /*
+        Example 1:
+
+Input: word = "abcdefd", ch = "d"
+Output: "dcbaefd"
+Explanation: The first occurrence of "d" is at index 3.
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+
+Example 2:
+
+Input: word = "xyxzxe", ch = "z"
+Output: "zxyxxe"
+Explanation: The first and only occurrence of "z" is at index 3.
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+
+Example 3:
+
+Input: word = "abcd", ch = "z"
+Output: "abcd"
+Explanation: "z" does not exist in word.
+You should not do any reverse operation, the resulting string is "abcd".
+         */
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter word");
+        String word=sc.nextLine();
+        System.out.println("Enter char");
+        char ch=sc.next().charAt(0);
+        System.out.println("result is : "+reversePrefix(word,ch));
+    }
+
+    public static String reversePrefix(String word, char ch) {
+        if(!(word.contains(Character.toString(ch)))){
+            return word;
+        }
+        Character c1 = word.chars().mapToObj(c -> (char) c).filter(c->c==ch).findFirst().get();
+        String substring = word.substring(0, word.indexOf(c1)+1);
+        String reversedSubstring=new StringBuilder(substring).reverse().toString();
+        String rest=word.substring(reversedSubstring.length(),word.length());
+        return reversedSubstring+rest;
     }
 
 }
