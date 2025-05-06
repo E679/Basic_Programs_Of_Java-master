996f46d870f4b3f96ac3cc3bdd7411c36682c2f7
Supreeth <saisupreethgoud4@gmail.com>
2025-05-06 16:23:44 +0530

    ReverseWithSpacesAndSameLength

---
+++ Basic_Programs_Of_Java-master/src/Strings/ReverseWithSpacesAndSameLength.java
@@ -0,0 +1,67 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseWithSpacesAndSameLength {
+    public static void main(String[] args) {
+        String input = "my name is supreeth";
+        String result = reversePreserveSpaces(input);
+        System.out.println(result); // Output: oc tics ie manym
+        String s = reversePreservingFormat(input);
+        System.out.println(s);
+    }
+
+    public static String reversePreserveSpaces(String input) {
+        char[] inputArr = input.toCharArray();
+        char[] result = new char[inputArr.length];
+
+        // Step 1: Mark space positions in result
+        for (int i = 0; i < inputArr.length; i++) {
+            if (inputArr[i] == ' ') {
+                result[i] = ' ';
+            }
+        }
+
+        // Step 2: Reverse the characters (ignoring spaces)
+        int j = inputArr.length - 1;
+        for (int i = 0; i < inputArr.length; i++) {
+            if (inputArr[i] != ' ') {
+                while (result[j] == ' ') {
+                    j--;
+                }
+                result[j] = inputArr[i];
+                j--;
+            }
+        }
+
+        return new String(result);
+    }
+
+    //Another way
+    public static String reversePreservingFormat(String input) {
+        List<Character> letters = new ArrayList<>();
+
+        // Step 1: Collect all letters from the string
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                letters.add(c);
+            }
+        }
+
+        // Step 2: Create result by replacing letters in reverse order, skipping non-letters
+        StringBuilder result = new StringBuilder();
+
+        int letterIndex = letters.size() - 1;
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                result.append(letters.get(letterIndex--));
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+}
+
