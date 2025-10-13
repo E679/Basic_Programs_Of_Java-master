db6b481766db017fd03a167eb38f32f1ecbe0c24
Supreeth <saisupreethgoud4@gmail.com>
2025-10-13 22:20:36 +0530

    gcd

--- Basic_Programs_Of_Java-master/src/Arrays/subarraySum.java
+++ Basic_Programs_Of_Java-master/src/Arrays/subarraySum.java
@@ -5,14 +5,15 @@ import java.util.Map;
 
 public class subarraySum {
     public static void main(String[] args) {
-        int[] nums={3,2,1};
-        int k=3;
-        int c=subarraySumUptoKelements(nums,k);
-        System.out.println("Number of Subarrays are: "+c);
-        subarraySumUptoKelementsAnotherWay(new int[]{1,1,1},2);
-        System.out.println("Number of Subarrays are: "+c);
+        int[] nums = {3, 2, 1};
+        int k = 3;
+        int c = subarraySumUptoKelements(nums, k);
+        System.out.println("Number of Subarrays are: " + c);
+        subarraySumUptoKelementsAnotherWay(new int[]{1, 1, 1}, 2);
+        System.out.println("Number of Subarrays are: " + c);
 
     }
+
     private static int subarraySumUptoKelements(int[] nums, int k) {
         int count = 0;
 
@@ -30,17 +31,21 @@ public class subarraySum {
 
         return count;
     }
-    private static int subarraySumUptoKelementsAnotherWay(int[] nums, int k) {
-        int count=0;int sum=0;
-        Map<Integer, Integer> map=new HashMap<>();
-        map.put(0,1);
 
-        for(int num:nums){
-            sum+=num;
-            if(map.containsKey(sum-k)){
-                count+=map.get(sum-k);
+    //Time complexity is O(n2)
+    //Below is optimal solution with O(n)
+    private static int subarraySumUptoKelementsAnotherWay(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
             }
-            map.put(sum,map.getOrDefault(sum,0)+1);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
