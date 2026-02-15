26113f5b15a1a392ca538c3b71d8181cd89d7192
Supreeth <saisupreethgoud4@gmail.com>
2026-02-16 00:22:35 +0530

    MergeIntervals

--- Basic_Programs_Of_Java-master/src/Arrays/Complex/Level2_Common_in_Product_Companies/MergeIntervals.java
+++ Basic_Programs_Of_Java-master/src/Arrays/Complex/Level2_Common_in_Product_Companies/MergeIntervals.java
@@ -18,5 +18,49 @@ public class MergeIntervals {
         res.add(curr);
         return res.toArray(new int[res.size()][]);
     }
+    //another way
+    public static int[][] merge1(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+        // Sort by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        merged.add(currentInterval);
+
+        for (int[] interval : intervals) {
+            int currentEnd = currentInterval[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
+
+            if (currentEnd >= nextStart) {
+                // Overlapping - merge
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            } else {
+                // Non-overlapping - add new interval
+                currentInterval = interval;
+                merged.add(currentInterval);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[][] intervals = {{1,3}, {2,6}, {8,10}, {15,18}};
+        int[][] result2 = merge1(intervals);
+
+        System.out.println("Merged intervals:");
+        for (int[] interval : result2) {
+            System.out.println(Arrays.toString(interval));
+        }
+        // Output: [1,6], [8,10], [15,18]
+    }
 }
 //Time: O(n log n), Space: O(n)
--- Basic_Programs_Of_Java-master/src/Leetcode/MergeIntervals.java
+++ Basic_Programs_Of_Java-master/src/Leetcode/MergeIntervals.java
@@ -1,7 +1,8 @@
 package Leetcode;
 
 import java.util.*;
-
+//Merge Intervals
+//Problem: Given a collection of intervals, merge overlapping intervals.
 public class MergeIntervals {
     public static int[][] mergeIntervals(int[][] interval1, int[][] interval2) {
         List<int[]> intervals = new ArrayList<>();
@@ -24,6 +25,38 @@ public class MergeIntervals {
         return merged.toArray(new int[merged.size()][]);
     }
 
+    //another way
+        public static int[][] merge(int[][] intervals) {
+            if (intervals.length <= 1) {
+                return intervals;
+            }
+
+            // Sort by start time
+            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+            List<int[]> merged = new ArrayList<>();
+            int[] currentInterval = intervals[0];
+            merged.add(currentInterval);
+
+            for (int[] interval : intervals) {
+                int currentEnd = currentInterval[1];
+                int nextStart = interval[0];
+                int nextEnd = interval[1];
+
+                if (currentEnd >= nextStart) {
+                    // Overlapping - merge
+                    currentInterval[1] = Math.max(currentEnd, nextEnd);
+                } else {
+                    // Non-overlapping - add new interval
+                    currentInterval = interval;
+                    merged.add(currentInterval);
+                }
+            }
+
+            return merged.toArray(new int[merged.size()][]);
+        }
+
+
     public static void main(String[] args) {
         int[][] interval1 = {{2, 4}, {5, 6}, {7, 9}};
         int[][] interval2 = {{2, 3}, {5, 6}, {7, 8}, {10, 12}};
@@ -33,6 +66,15 @@ public class MergeIntervals {
         for (int[] interval : result) {
             System.out.print(Arrays.toString(interval));
         }
+
+        int[][] intervals = {{1,3}, {2,6}, {8,10}, {15,18}};
+        int[][] result2 = merge(intervals);
+
+        System.out.println("Merged intervals:");
+        for (int[] interval : result2) {
+            System.out.println(Arrays.toString(interval));
+        }
+        // Output: [1,6], [8,10], [15,18]
     }
 }
 
