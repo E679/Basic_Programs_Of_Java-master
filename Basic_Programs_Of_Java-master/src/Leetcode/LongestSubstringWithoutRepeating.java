ce2e6d24b2b9eb42b3de657afab9b2cd39618d4a
Supreeth <saisupreethgoud4@gmail.com>
2026-02-16 01:11:02 +0530

    LongestSubstringWithoutRepeating

--- Basic_Programs_Of_Java-master/src/Leetcode/LongestSubstringWithoutRepeating.java
+++ Basic_Programs_Of_Java-master/src/Leetcode/LongestSubstringWithoutRepeating.java
@@ -30,6 +30,24 @@ public class LongestSubstringWithoutRepeating {
         return maxLength;
     }
 
+
+    // Longest substring without repeating characters
+    public int lengthOfLongestSubstring1(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLen = 0, start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                start = Math.max(start, map.get(c) + 1);
+            }
+            map.put(c, end);
+            maxLen = Math.max(maxLen, end - start + 1);
+        }
+        return maxLen;
+    }
+
+
     //Another way
     public static int findLongestSubstringLength(String s) {
         if (s == null || s.length() == 0) {
